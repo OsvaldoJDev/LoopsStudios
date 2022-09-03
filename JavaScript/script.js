9ccc67f09a1c111ee829ca1e880ecc4732d61c51
Osvaldo JDev <Wosvaldojesus44@gmail.com>
2022-09-03 11:46:41 -0300

    Adições e melhorias no CSS e JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,19 @@
 const menuDropDown = document.querySelector(".drop-down");
 const btnIcon = document.querySelector(".btn-menu img");
+const btnMobile = document.querySelector(".btn-menu");
 
-function abrirMenu() {
+function abrirMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+
   menuDropDown.classList.toggle("on");
   if (btnIcon.getAttribute("src") === "./img/icon-hamburger.svg") {
     btnIcon.src = "./img/icon-close.svg";
   } else {
     btnIcon.src = "./img/icon-hamburger.svg";
   }
+  const MenuAtivo = menuDropDown.classList.contains("on");
+  event.currentTarget.setAttribute("aria-expanded", MenuAtivo);
 }
+
+btnMobile.addEventListener("click", abrirMenu);
+btnMobile.addEventListener("touchstart", abrirMenu);
